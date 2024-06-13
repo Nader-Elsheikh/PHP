8548e968d5733a8a96cb15610d17e27ee225524a
Nader Elsheikh <nadermahmoud618@gmail.com>
2024-06-13 03:51:12 +0300

    Update

---
+++ RealEstate-PHP/admin/config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$con = mysqli_connect("localhost","root","NMelsheikh@0","realestatephp");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+?>
